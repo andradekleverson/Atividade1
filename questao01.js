aa6a89e84d18953660bc98b549c9fc6ce3743ff9
andradekleverson <andrade.kleverson@academico.ifpb.edu.br>
2026-09-01 15:13:20 -0300

    Add files via upload

---
+++ questao01.js
@@ -0,0 +1,20 @@
+
+// 5+3 -> produz 8
+
+// 10>5 -> produz True
+
+// "Olá " + "mundo" -> produz "Olá mundo"
+
+let mensagem = "Olá "+"mundo";
+
+let somar = function(a,b) {
+	return a+b;
+};
+
+let resultado = somar(5,3);
+
+console.log(subtracao(5,3));
+
+function subtracao(a,b) {
+	return a-b;
+};
